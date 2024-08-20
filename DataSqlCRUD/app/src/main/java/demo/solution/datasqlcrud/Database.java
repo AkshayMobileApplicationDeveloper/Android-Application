70d8a625c644582af870db62b78c012b691e5c87
AkshayMobileApplicationDeveloper <akshaykumarprajapati@hotmail.com>
2024-08-21 01:53:30 +0530

    hello

---
+++ DataSqlCRUD/app/src/main/java/demo/solution/datasqlcrud/Database.java
@@ -0,0 +1,26 @@
+package demo.solution.datasqlcrud;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class Database extends SQLiteOpenHelper {
+
+    public static final String dbname="Signup.db";
+
+    public Database(@Nullable Context context) {
+        super(context,dbname,null,1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
